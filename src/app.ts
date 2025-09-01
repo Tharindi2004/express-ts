edc5d52e74946e41fe4b5a1231c6b7145b5a275c
Your NameTharindi <tharamt6@gmail.com>
2025-09-01 11:07:07 +0530

    implemented routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application } from "express";
+import greetinRoute from "./routes/greeting.route";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use("/greeting", greetinRoute);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
